1aa7c7f645b3b9a4d04815339bd38a134e62f84f
LiudmilaLi <LiudmilaLitvin@gmail.com>
2019-10-09 21:00:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  
+  return BigInt(first) * BigInt(second) + ''; //первый вариант решения путем встраивания аргументов функции в объект BigInt  
 }
+
+ /* let firstNew = first.split('').reverse();   //второе решение
+  let secondNew = second.split('').reverse();
+  let multArr = [];
+  for (let i = 0; i < firstNew.length; i++) {
+    for (let j = 0; j < secondNew.length; j++) {
+      let total = firstNew[i] * secondNew[j]; 
+      multArr[i + j] = (multArr[i + j]) ? multArr[i + j] + total : total; }
+  }
+  for (let i = 0; i < multArr.length; i++) {
+    let num = multArr[i] % 10;
+    let round = Math.floor(multArr[i] / 10);
+    multArr[i] = num;
+    if (multArr[i+1])  multArr[i + 1] += round;
+    else if (round !== 0) multArr[i + 1] = round;
+  } 
+    return multArr.reverse().join('');
+}*/
